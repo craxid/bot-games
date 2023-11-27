89d24d2f959a9556a55c0835c4d7df1fc96b6e06
Dede K <dede.klender@gmail.com>
2023-11-27 13:38:36 +0700

    Create asahotak.js

---
+++ asahotak.js
@@ -0,0 +1,503 @@
+[
+  {
+    index: 0,
+    soal: 'Hewan sejenis kadal yang dapat merubah warna kulitnya?',
+    jawaban: 'Bunglon'
+  },
+  {
+    index: 1,
+    soal: 'Hewan sejenis kadal yang dapat merubah warna kulitnya?',
+    jawaban: 'Bunglon'
+  },
+  {
+    index: 2,
+    soal: 'Surat instruksi kepada Soeharto untuk mengambil alih keamanan Indonesia pada tanggal 11 Maret 1966?',
+    jawaban: 'Supersemar'
+  },
+  {
+    index: 3,
+    soal: 'Sungai terpanjang di Benua Amerika?',
+    jawaban: 'Amazon'
+  },
+  {
+    index: 4,
+    soal: 'Wanita yang tercatat dalam sejarah sebagai penjahit Bendera Pusaka Indonesia?',
+    jawaban: 'Fatmawati'
+  },
+  {
+    index: 5,
+    soal: 'Lomba lari jarak jauh sepanjang 42,195 km?',
+    jawaban: 'Marathon'
+  },
+  {
+    index: 6,
+    soal: 'Garis yang membagi Bumi menjadi dua bagian belahan utara dan belahan selatan?',
+    jawaban: 'Khatulistiwa'
+  },
+  {
+    index: 7,
+    soal: 'Kota yang berdiri sejak tahun 682 ini disebut sebagai kota tertua di Indonesia?',
+    jawaban: 'Palembang'
+  },
+  {
+    index: 8,
+    soal: 'Sebutan untuk gaya tarik-menarik yang terjadi di alam semesta termasuk Bumi?',
+    jawaban: 'Gravitasi'
+  },
+  {
+    index: 9,
+    soal: 'Nama film yang bercerita tentang robot yang dapat berubah menjadi kendaran?',
+    jawaban: 'Transformers'
+  },
+  {
+    index: 10,
+    soal: 'Petarung di zaman Romawi Kuno yang melakukan pertarungan untuk hiburan umum?',
+    jawaban: 'Gladiator'
+  },
+  {
+    index: 11,
+    soal: 'Nama kapal penumpang mewah yang tenggelam saat pelayaran perdana tahun 1912?',
+    jawaban: 'Titanic'
+  },
+  {
+    index: 12,
+    soal: 'Klub sepak bola dari Italia yang memiliki julukan Nyonya Tua?',
+    jawaban: 'Juventus'
+  },
+  {
+    index: 13,
+    soal: 'Mekanisme sistem pemerintahan negara yang berdasarkan kehendak rakyat?',
+    jawaban: 'Demokrasi'
+  },
+  {
+    index: 14,
+    soal: 'Nama kesenian drama tari tradisional yang khas dari Bali?',
+    jawaban: 'Kecak'
+  },
+  {
+    index: 15,
+    soal: 'Nama mata uang yang digunakan di negara Rusia?',
+    jawaban: 'Rubel'
+  },
+  {
+    index: 16,
+    soal: 'Ilmu alam yang mengamati benda langit serta fenomena di luar atmosfer Bumi?',
+    jawaban: 'Astronomi'
+  },
+  {
+    index: 17,
+    soal: 'Kaisar Prancis yang menguasai Benua Eropa pada tahun 1803-1815?',
+    jawaban: 'Napoleon'
+  },
+  {
+    index: 18,
+    soal: 'Seniman terkenal dunia dari Spanyol dengan aliran seni bernama kubisme?',
+    jawaban: 'Picasso'
+  },
+  {
+    index: 19,
+    soal: 'Nama panglima tertinggi Pasukan Sekutu pada Perang dunia II?',
+    jawaban: 'Eisenhower'
+  },
+  {
+    index: 20,
+    soal: 'Senjata fiksi dalam dunia Star Wars, digunakan oleh Jedi dan Sith?',
+    jawaban: 'Lightsaber'
+  },
+  {
+    index: 21,
+    soal: 'Nama semenanjung di Eropa dimana terdapat negara Norwegia dan Swedia?',
+    jawaban: 'Skandinavia'
+  },
+  {
+    index: 22,
+    soal: 'Stadion sepak bola di London yang juga markas utama klub Arsenal FC?',
+    jawaban: 'Emirates'
+  },
+  {
+    index: 23,
+    soal: 'Penyanyi wanita terkenal dunia, dulu berada dalam grup Destiny’s Child?',
+    jawaban: 'Beyonce'
+  },
+  {
+    index: 24,
+    soal: 'Istilah untuk sebuah kepercayaan bahwa Tuhan adalah satu atau tunggal?',
+    jawaban: 'Monoteisme'
+  },
+  {
+    index: 25,
+    soal: 'Nama kerajaan dimana rajanya menjadi wakil Gubernur DI Yogyakarta saat ini?',
+    jawaban: 'Pakualaman'
+  },
+  {
+    index: 26,
+    soal: 'Disebut sebagai salah satu matematikawan terbesar sejarah, lahir pada 287 SM?',
+    jawaban: 'Archimedes'
+  },
+  {
+    index: 27,
+    soal: 'Rangkaian pegunungan tertinggi di Indonesia, terletak di Pulau Papua?',
+    jawaban: 'Jayawijaya'
+  },
+  {
+    index: 28,
+    soal: 'Kelompok fiksi superhero yang terdiri dari Iron Man, Captain America, Hulk, Thor?',
+    jawaban: 'Avengers'
+  },
+  {
+    index: 29,
+    soal: 'Nama wilayah luas di Rusia, meliputi hampir seluruh wilayah Asia Utara?',
+    jawaban: 'Siberia'
+  },
+  {
+    index: 30,
+    soal: 'Suatu benda atau media yang menghantarkan arus listrik dengan mudah?',
+    jawaban: 'Konduktor'
+  },
+  {
+    index: 31,
+    soal: 'Nama bahasa yang digunakan di Iran, Tajikistan, Afganistan, dan Uzbekistan?',
+    jawaban: 'Farsi'
+  },
+  {
+    index: 32,
+    soal: 'Daerah dimana terdapat praktik pertanian pertama sekitar tahun 8000 SM?',
+    jawaban: 'Mesopotamia'
+  },
+  {
+    index: 33,
+    soal: 'Ideologi yang berdasarkan pada prinsip kepemimpinan dengan otoritas absolut?',
+    jawaban: 'Fasisme'
+  },
+  {
+    index: 34,
+    soal: 'Nama periode geologi dari 252 ke 66 juta tahun yang lalu, disebut juga Zaman Reptil?',
+    jawaban: 'Mesozoikum'
+  },
+  {
+    index: 35,
+    soal: 'Nama yang dikenal dalam legenda Jawab sebagai arsitek perancang Candi Borobudur?',
+    jawaban: 'Gunadharma'
+  },
+  {
+    index: 36,
+    soal: 'Tarian tradisional Tionghoa yang menggunakan kostum menyerupai singa',
+    jawaban: 'Barongsai'
+  },
+  {
+    index: 37,
+    soal: 'Istilah yang artinya perpindahan penduduk dari desa ke kota?',
+    jawaban: 'Urbanisasi'
+  },
+  {
+    index: 38,
+    soal: 'Grup musik terkenal tahun 1970an asal Bandung, personelnya tiga bersaudara?',
+    jawaban: 'Bimbo'
+  },
+  {
+    index: 39,
+    soal: 'Nama film aksi laga Indonesia tahun 2009 yang dibintangi oleh Iko Uwais?',
+    jawaban: 'Merantau'
+  },
+  {
+    index: 40,
+    soal: 'Dikenal sebagai “maestro keroncong Indonesia”, pencipta lagu Bengawan Solo?',
+    jawaban: 'Gesang'
+  },
+  {
+    index: 41,
+    soal: 'Skala suhu dimana titik beku air berada pada 0 derajat dan titik didih pada 100 derajat?',
+    jawaban: 'Celcius'
+  },
+  {
+    index: 42,
+    soal: 'Makhluk halus mitos Jawa, berwujud manusia mirip kera, bertubuh besar dan berbulu?',
+    jawaban: 'Genderuwo'
+  },
+  {
+    index: 43,
+    soal: 'Sebuah distrik di Los Angeles yang terkenal dengan industri perfilmannya yang mendunia?',
+    jawaban: 'Hollywood'
+  },
+  {
+    index: 44,
+    soal: 'Cabang biologi yang mempelajari pewarisan sifat pada organisme maupun suborganisme?',
+    jawaban: 'Genetika'
+  },
+  {
+    index: 45,
+    soal: 'Situs manusia purba di Jawa Tengah, juga merupakan Situs Warisan Dunia UNESCO?',
+    jawaban: 'Sangiran'
+  },
+  {
+    index: 46,
+    soal: 'Bongkahan batu yang berukuran lebih kecil daripada planet, mengorbit Matahari?',
+    jawaban: 'Asteroid'
+  },
+  {
+    index: 47,
+    soal: 'Istilah untuk hewan pemakan rerumputan atau tumbuhan?',
+    jawaban: 'Herbivora'
+  },
+  {
+    index: 48,
+    soal: 'Istilah yang berarti pembantaian sistematis terhadap suatu suku atau kelompok?',
+    jawaban: 'Genosida'
+  },
+  {
+    index: 49,
+    soal: 'Nama penyakit demam yang ditularkan oleh nyamuk Anopheles betina?',
+    jawaban: 'Malaria'
+  },
+  {
+    index: 50,
+    soal: 'Nama tokoh fiksi budaya Sunda yang sifatnya lucu, polos, tetapi sekaligus cerdas?',
+    jawaban: 'Kabayan'
+  },
+  {
+    index: 51,
+    soal: 'Asrama tempat belajar para siswa santri yang dibimbin oleh Kiai?',
+    jawaban: 'Pesantren'
+  },
+  {
+    index: 52,
+    soal: 'Perusahaan video game dari Jepang, pencipta Mario Bros dan Pokemon?',
+    jawaban: 'Nintendo'
+  },
+  {
+    index: 53,
+    soal: 'Sebuah agama resmi di Indonesia yang berdasarkan pada ajaran Konfusius?',
+    jawaban: 'Khong hu cu'
+  },
+  {
+    index: 54,
+    soal: 'Nama ensiklopedia online yang bebas ditulis dan disunting oleh siapa saja?',
+    jawaban: 'Wikipedia'
+  },
+  {
+    index: 55,
+    soal: 'Bangun datar segi empat, sepasang sisi sejajar namun tidak sama panjang?',
+    jawaban: 'Trapesium'
+  },
+  {
+    index: 56,
+    soal: 'Penyakit dimana hormon pengatur gula dara dari tubuh tidak mencukupi?',
+    jawaban: 'Diabetes'
+  },
+  {
+    index: 57,
+    soal: 'Negara independen terkecil di dunia yang diakui secara internasional?',
+    jawaban: 'Vatikan'
+  },
+  {
+    index: 58,
+    soal: 'Kekaisaran kuno di wilayah Yunani yang dipimpin oleh Aleksander Agung?',
+    jawaban: 'Makedonia'
+  },
+  {
+    index: 59,
+    soal: 'Negara dengan jumlah penduduk Muslim terbanyak kedua di dunia?',
+    jawaban: 'Pakistan'
+  },
+  {
+    index: 60,
+    soal: 'Gejala penurunan fungsi otak, umumnya terjadi pada usia lanjut?',
+    jawaban: 'Demensia'
+  },
+  {
+    index: 61,
+    soal: 'Bahan bakar fosil, digunakan di Pembangkit Listrik Tenaga Uap?',
+    jawaban: 'Batu bara'
+  },
+  {
+    index: 62,
+    soal: 'Nama pura terkenal di Bali yang dibangun pada ujung batu karang?',
+    jawaban: 'Uluwatu'
+  },
+  {
+    index: 63,
+    soal: 'Sebuah gelar yang diperoleh putra Sultan Agung dari Mataram?',
+    jawaban: 'Amangkurat'
+  },
+  {
+    index: 64,
+    soal: 'Seorang yang bertugas merawat bayi atau anak pada suatu keluarga?',
+    jawaban: 'Pramusiwi'
+  },
+  {
+    index: 65,
+    soal: 'Daerah di Jepang dimana terjadi kebocoran nuklir pada tahun 2011?',
+    jawaban: 'Fukushima'
+  },
+  {
+    index: 66,
+    soal: 'Perlindungan finansial untuk jiwa kesehatan properti dsb?',
+    jawaban: 'Asuransi'
+  },
+  {
+    index: 67,
+    soal: 'Perusahaan berlogo kuda asal Italia produsen mobil super dan mobil balap?',
+    jawaban: 'Ferrari'
+  },
+  {
+    index: 68,
+    soal: 'Suku dari sulawesi selatan yang terkenal sebagai pelaut ulung?',
+    jawaban: 'Bugis'
+  },
+  {
+    index: 69,
+    soal: 'Vampir yang merupakan tokoh utama fiksi ciptaan Bram Stoker?',
+    jawaban: 'Drakula'
+  },
+  {
+    index: 70,
+    soal: 'Planet ini merupakan planet terbesar di Tata Surya?',
+    jawaban: 'Yupiter'
+  },
+  {
+    index: 71,
+    soal: 'Tanaman yang dipakai sebagai bahan pembuat kertas pada Zaman Mesir Kuno?',
+    jawaban: 'Papirus'
+  },
+  {
+    index: 72,
+    soal: 'Bahan peledak berupa bubuk campuran dari belerang, arang, dan kalium nitrat?',
+    jawaban: 'Mesiu'
+  },
+  {
+    index: 73,
+    soal: 'Bahasa resmi yang juga digunakan secara luas di Negara Filipina?',
+    jawaban: 'Tagalog'
+  },
+  {
+    index: 74,
+    soal: 'Negara kecil di Amerika Selatan, terdapat banyak keturunan Suku Jawa?',
+    jawaban: 'Suriname'
+  },
+  {
+    index: 75,
+    soal: 'Kuda jantan bersayap dari mitologi Yunani?',
+    jawaban: 'Pegasus'
+  },
+  {
+    index: 76,
+    soal: 'Grup musik Rock alternatif dari London. vokalisnya bernama Chris Martin?',
+    jawaban: 'Coldplay'
+  },
+  {
+    index: 77,
+    soal: 'Buah yang jika dipotong mempunyai penampang berbentuk bintang?',
+    jawaban: 'Belimbing'
+  },
+  {
+    index: 78,
+    soal: 'Ensembel musik yang biasanya menonjolkan metalofon, gambang, gendang, dan gong?',
+    jawaban: 'Gamelan'
+  },
+  {
+    index: 79,
+    soal: 'Nama kota yang selama beberapa abad diperebutkan oleh Islam, Kristen, Yahudi?',
+    jawaban: 'Yerusalem'
+  },
+  {
+    index: 80,
+    soal: 'Bahasa resmi negara Bangladesh, digunakan oleh sekitar 200 juta orang?',
+    jawaban: 'Bengali'
+  },
+  {
+    index: 81,
+    soal: 'Minuman memabukkan yang berbahan dasar etanol?',
+    jawaban: 'Alkohol'
+  },
+  {
+    index: 82,
+    soal: 'Satu-satunya gunung berapi aktif di Eropa Daratan yang letaknya di Italia?',
+    jawaban: 'Vesuvius'
+  },
+  {
+    index: 83,
+    soal: 'Karakter fiksi berambut sangat panjang tokoh utama film animasi Tangled?',
+    jawaban: 'Rapunzel'
+  },
+  {
+    index: 84,
+    soal: 'Bencana alam berupa gelombang ombak besar dari laut?',
+    jawaban: 'Tsunami'
+  },
+  {
+    index: 85,
+    soal: 'Nama kota di negara Turki yang zaman dulu bernama Konstantinopel?',
+    jawaban: 'Istanbul'
+  },
+  {
+    index: 86,
+    soal: 'Subspesies manusia yang punah dari muka bumi sekitar 30.000 tahun yang lalu?',
+    jawaban: 'Neanderthal'
+  },
+  {
+    index: 87,
+    soal: 'Nama ritual bunuh diri kaum Samurai di Jepang dengan cara merobek perut?',
+    jawaban: 'Seppuku'
+  },
+  {
+    index: 88,
+    soal: 'Jenis keju asal Italia yang seringkali terdapat pada hidangan pizza?',
+    jawaban: 'Mozzarella'
+  },
+  {
+    index: 89,
+    soal: 'Gelar bagi istri dari penguasa monarki pria(raja, sultan, atau kaisar)?',
+    jawaban: 'Permaisuri'
+  },
+  {
+    index: 90,
+    soal: 'Nama suku yang sejak lama mendiami daerah kutub utara bumi?',
+    jawaban: 'Eskimo'
+  },
+  {
+    index: 91,
+    soal: 'Unsur paling berlimpah kedua setelah oksigen pada tubuh manusia?',
+    jawaban: 'Karbon'
+  },
+  {
+    index: 92,
+    soal: 'Peristiwa genosida terhadap penganut Yahudi Eropa selama Perang Dunia II?',
+    jawaban: 'Holokaus'
+  },
+  {
+    index: 93,
+    soal: 'Proses perubahan gula menjadi etanol yang dilakukan oleh ragi?',
+    jawaban: 'Fermentasi'
+  },
+  {
+    index: 94,
+    soal: 'Ramalan dalam tradisi jawa yang membahas mengenai adanya ratu adil?',
+    jawaban: 'Jayabaya'
+  },
+  {
+    index: 95,
+    soal: 'Tokoh abad ke 15 dari Polandia yang mencetuskan teori heliosentrisme?',
+    jawaban: 'Copernicus'
+  },
+  {
+    index: 96,
+    soal: 'Alat populer untuk menyambung dua sisi kain, digunakan dalam pakaian, tas, dsb?',
+    jawaban: 'Ritsleting'
+  },
+  {
+    index: 97,
+    soal: 'Satuan perbedaan potensi listrik antara dua titik dalam rangkaian listrik?',
+    jawaban: 'Voltase'
+  },
+  {
+    index: 98,
+    soal: 'Sistem sosial yang memberikan kekuasaan besar kepada golongan bangsawan?',
+    jawaban: 'Feodalisme'
+  },
+  {
+    index: 99,
+    soal: 'Nama pemanis buatan/sintetis yang tersusun dari dua macam asam amino?',
+    jawaban: 'Aspartam'
+  },
+  ... 128 more items
+]
